40809f0efb54fb6a9bcd43f43a8f0a7c01515728
Joao Pinto <joaopintotovar2000@gmail.com>
2021-03-05 12:56:22 -0400

    arregalo de bugs por git de mierda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ServicesComponent } from './services/services.component';
 import { PagesComponent } from './pages/pages.component';
 import { RecurrentComponent } from './recurrent/recurrent.component';
-=======
 import { ComponentsComponent } from './components/components.component';
-<<<<<<< HEAD
->>>>>>> 9baccc065403e8c4dfde1874b90b54e04e711c28
-=======
 import { HomeComponent } from './pages/home/home.component';
 import { ListapelisComponent } from './pages/listapelis/listapelis.component';
 import { DetallecadapeliComponent } from './pages/detallecadapeli/detallecadapeli.component';
@@ -22,22 +17,14 @@ import { RegisterComponent } from './pages/register/register.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PostcardComponent } from './components/postcard/postcard.component';
-import { ServicesComponent } from './services/services.component';
 import { ModelsComponent } from './models/models.component';
->>>>>>> dev
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     ServicesComponent,
     PagesComponent,
-    RecurrentComponent
-=======
-    ComponentsComponent
->>>>>>> 9baccc065403e8c4dfde1874b90b54e04e711c28
-=======
+    RecurrentComponent,
     ComponentsComponent,
     HomeComponent,
     ListapelisComponent,
@@ -51,7 +38,6 @@ import { ModelsComponent } from './models/models.component';
     PostcardComponent,
     ServicesComponent,
     ModelsComponent
->>>>>>> dev
   ],
   imports: [
     BrowserModule,
